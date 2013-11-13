97e1a86866454f422b1425c966817aeaa676ac6b
Jan Drewniak <jan.drewniak@gmail.com>
2013-11-13 11:56:25 -0500

    moving CNAME file

--- javascripts/drafts/random_scribbles.js
+++ javascripts/drafts/random_scribbles.js
@@ -1,4 +1,183 @@
 /* Author: Poops Drewniak
     
 */
-function onDocumentMouseMove(e){ballSprite.position.set(e.clientX,e.clientY,0),mouse.x=e.clientX/window.innerWidth*2-1,mouse.y=-(e.clientY/window.innerHeight)*2+1,update()}function buildAxes(e){var t=new THREE.Object3D;return t.add(buildAxis(new THREE.Vector3(0,0,0),new THREE.Vector3(e,0,0),16711680,!1)),t.add(buildAxis(new THREE.Vector3(0,0,0),new THREE.Vector3(-e,0,0),16711680,!0)),t.add(buildAxis(new THREE.Vector3(0,0,0),new THREE.Vector3(0,e,0),65280,!1)),t.add(buildAxis(new THREE.Vector3(0,0,0),new THREE.Vector3(0,-e,0),65280,!0)),t.add(buildAxis(new THREE.Vector3(0,0,0),new THREE.Vector3(0,0,e),255,!1)),t.add(buildAxis(new THREE.Vector3(0,0,0),new THREE.Vector3(0,0,-e),255,!0)),t}function buildAxis(e,t,n,r){var i=new THREE.Geometry,s;r?s=new THREE.LineDashedMaterial({linewidth:3,color:n,dashSize:3,gapSize:3}):s=new THREE.LineBasicMaterial({linewidth:3,color:n}),i.vertices.push(e.clone()),i.vertices.push(t.clone()),i.computeLineDistances();var o=new THREE.Line(i,s,THREE.LinePieces);return o}function animate(){requestAnimationFrame(animate),renderer.render(scene,camera)}function addCube(){var e=new THREE.CubeGeometry(50,50,50),t=new THREE.MeshBasicMaterial,n=new THREE.Mesh(e,t);n.position.set(see.x,see.y,0),scene.add(n),console.log("addCube")}function update(){var e=new THREE.Vector3(mouse.x,mouse.y,1);projector.unprojectVector(e,camera);var t=new THREE.Raycaster(camera.position,e.sub(camera.position).normalize());console.log("cube.y"+cube.position.y),console.log("ray.y"+t.ray.direction.y),see.x=t.ray.direction.x*1e3,see.y=t.ray.direction.y*1e3,addCube()}var camera,scene,renderer,projector;scene=new THREE.Scene;var mouse={x:0,y:0},VIEW_ANGLE=45,ASPECT=$(window).width()/$(window).height(),NEAR=.1,FAR=2e4;camera=new THREE.PerspectiveCamera(VIEW_ANGLE,ASPECT,NEAR,FAR),scene.add(camera),camera.position.set(0,100,800),camera.lookAt(scene.position),camera.lookAt(scene.position);var size=500,step=50,geometry=new THREE.Geometry;for(var i=-size;i<=size;i+=step)geometry.vertices.push(new THREE.Vector3(-size,0,i)),geometry.vertices.push(new THREE.Vector3(size,0,i)),geometry.vertices.push(new THREE.Vector3(i,0,-size)),geometry.vertices.push(new THREE.Vector3(i,0,size));var material=new THREE.LineBasicMaterial({color:0,opacity:.2}),line=new THREE.Line(geometry,material);line.type=THREE.LinePieces,scene.add(line);var axes=buildAxes(1e3);scene.add(axes);var ballTexture=THREE.ImageUtils.loadTexture("icons/apple-touch-icon-144x144-precomposed.png");ballSprite=new THREE.Sprite({map:ballTexture,useScreenCoordinates:!0,alignment:THREE.SpriteAlignment.center}),ballSprite.position.set(50,50,0),scene.add(ballSprite),projector=new THREE.Projector,document.addEventListener("mousemove",onDocumentMouseMove,!1),renderer=new THREE.CanvasRenderer,renderer.setSize(window.innerWidth,window.innerHeight);var range=50;geom=new THREE.CubeGeometry(50,50,50);var grayness=Math.random()*.5+.25;mat=new THREE.MeshBasicMaterial,cube=new THREE.Mesh(new THREE.CubeGeometry(50,50,50),new THREE.MeshNormalMaterial),mat.color.setRGB(grayness,grayness,grayness),cube.position.set(0,0,0),cube.rotation.set(Math.random(),Math.random(),Math.random()).multiplyScalar(2*Math.PI),cube.grayness=grayness,$("body").append(renderer.domElement),$(document).keydown(function(e){console.log("x:"+camera.position.x+" y:"+camera.position.y+" z:"+camera.position.z);switch(e.keyCode){case 38:console.log(camera.position.x),camera.position.y+=10;break;case 40:camera.position.y-=10;break;case 37:camera.position.x-=10;break;case 39:camera.position.x+=10}renderer.render(scene,camera)}),$("body").css("overflow","hidden"),camera.lookAt(scene.position),renderer.render(scene,camera),animate();var see={};see.x=mouse.x,see.y=mouse.y;
+
+
+
+var camera, 
+    scene, 
+    renderer,
+    projector;
+
+    scene = new THREE.Scene();
+
+var mouse = { x: 0, y: 0 };
+
+    var VIEW_ANGLE = 45, ASPECT = $(window).width() / $(window).height(), NEAR = 0.1, FAR = 20000;
+    camera = new THREE.PerspectiveCamera( VIEW_ANGLE, ASPECT, NEAR, FAR);
+    scene.add(camera);
+    camera.position.set(0,100,800);
+    camera.lookAt(scene.position);  
+
+    
+    camera.lookAt(scene.position);  
+
+    var size = 500, step = 50;
+    var geometry = new THREE.Geometry();
+    
+    for ( var i = - size; i <= size; i += step ) {
+        geometry.vertices.push( new THREE.Vector3( - size, 0, i ) );
+        geometry.vertices.push( new THREE.Vector3(   size, 0, i ) );
+        geometry.vertices.push( new THREE.Vector3( i, 0, - size ) );
+        geometry.vertices.push( new THREE.Vector3( i, 0,   size ) );
+    }
+
+    var material = new THREE.LineBasicMaterial( { color: 0x000000, opacity: 0.2 } );
+    var line = new THREE.Line( geometry, material );
+    line.type = THREE.LinePieces;
+    scene.add( line );
+
+    var axes = buildAxes( 1000 );
+    scene.add( axes );
+    
+    var ballTexture = THREE.ImageUtils.loadTexture( 'icons/apple-touch-icon-144x144-precomposed.png' );
+    
+    ballSprite = new THREE.Sprite( { map: ballTexture, useScreenCoordinates: true, alignment: THREE.SpriteAlignment.center } );
+    ballSprite.position.set( 50, 50, 0 );
+    
+    scene.add( ballSprite );
+
+    projector = new THREE.Projector();
+    
+    document.addEventListener('mousemove', onDocumentMouseMove, false);
+
+    renderer = new THREE.CanvasRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    
+
+    function onDocumentMouseMove(event){
+        ballSprite.position.set( event.clientX, event.clientY, 0 );
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+        update()
+
+    }
+
+
+
+        var range = 50;    
+        geom = new THREE.CubeGeometry( 50, 50, 50 );
+
+        var grayness = Math.random() * 0.5 + 0.25
+        mat = new THREE.MeshBasicMaterial();
+    cube = new THREE.Mesh( new THREE.CubeGeometry( 50, 50, 50 ), new THREE.MeshNormalMaterial() );
+        mat.color.setRGB( grayness, grayness, grayness );
+        cube.position.set( 0, 0, 0 );
+        cube.rotation.set( Math.random(), Math.random(), Math.random() ).multiplyScalar( 2 * Math.PI );
+        cube.grayness = grayness; // *** NOTE THIS
+    //    scene.add( cube );
+
+
+
+
+
+
+    //container.appendChild( renderer.domElement );
+    $('body').append(renderer.domElement)
+    //
+
+    $(document).keydown(function(e) {
+        console.log('x:'+camera.position.x+" y:"+camera.position.y+" z:"+camera.position.z)
+
+        switch ( e.keyCode ){ 
+        case 38:
+            console.log(camera.position.x)
+            camera.position.y += 10;
+            break;
+
+        case 40:
+            camera.position.y -= 10;
+            break;
+        case 37:
+            camera.position.x -= 10;
+            break;
+        case 39: 
+            camera.position.x += 10;        
+        }
+            renderer.render( scene, camera);
+ 
+    });
+
+    $('body').css('overflow', 'hidden');
+
+    camera.lookAt( scene.position );
+    renderer.render( scene, camera);
+
+    function buildAxes( length ) {
+        var axes = new THREE.Object3D();
+
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( length, 0, 0 ), 0xFF0000, false ) ); // +X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( -length, 0, 0 ), 0xFF0000, true) ); // -X
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, length, 0 ), 0x00FF00, false ) ); // +Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, -length, 0 ), 0x00FF00, true ) ); // -Y
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, length ), 0x0000FF, false ) ); // +Z
+        axes.add( buildAxis( new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, -length ), 0x0000FF, true ) ); // -Z
+
+        return axes;
+
+    }
+
+    function buildAxis( src, dst, colorHex, dashed ) {
+        var geom = new THREE.Geometry(),
+            mat; 
+
+        if(dashed) {
+            mat = new THREE.LineDashedMaterial({ linewidth: 3, color: colorHex, dashSize: 3, gapSize: 3 });
+        } else {
+            mat = new THREE.LineBasicMaterial({ linewidth: 3, color: colorHex });
+        }
+
+        geom.vertices.push( src.clone() );
+        geom.vertices.push( dst.clone() );
+        geom.computeLineDistances(); // This one is SUPER important, otherwise dashed lines will appear as simple plain lines
+
+        var axis = new THREE.Line( geom, mat, THREE.LinePieces );
+
+        return axis;
+
+    }
+
+    function animate() {
+
+        // note: three.js includes requestAnimationFrame shim
+        requestAnimationFrame( animate );
+        renderer.render( scene, camera ); 
+    }
+
+    animate()
+    
+    function addCube(){
+        var go = new THREE.CubeGeometry( 50, 50, 50 );
+        var ma = new THREE.MeshBasicMaterial();
+        var cub = new THREE.Mesh( go, ma );
+        cub.position.set( see.x, see.y, 0 );
+        scene.add( cub );
+        console.log('addCube')
+    }
+    var see = {}
+    see.x = mouse.x
+    see.y = mouse.y
+
+    function update(){
+        var vector = new THREE.Vector3( mouse.x, mouse.y, 1 );
+        projector.unprojectVector( vector, camera );
+        //console.log(vector)
+        var ray = new THREE.Raycaster( camera.position, vector.sub( camera.position ).normalize() );
+        console.log('cube.y'+cube.position.y )
+        console.log('ray.y'+ray.ray.direction.y)
+        see.x = ray.ray.direction.x*1000
+        see.y = ray.ray.direction.y*1000
+        //cube.position.z = 0
+        addCube();
+    }
+;
--- javascripts/my_face.js
+++ javascripts/my_face.js
@@ -1,1 +1,206 @@
-$(document).ready(function(){function h(e){u=e.getContext("2d");var r=document.getElementById("my_face_avatar");u.drawImage(r,0,0,t,n),p(u)}function p(e){var r=e.getImageData(0,0,t,n),i=r.data,o=0;for(y=0;y<n;y++){var u=[];for(x=0;x<t;x++){var f=Math.floor(Math.random()*255),l=(y*t+x)*4,h=i[l],p=i[l+1],d=i[l+2],v=c,m="rgb("+Math.floor(Math.random()*100).toString()+","+Math.floor(Math.random()*100).toString()+","+Math.floor(Math.random()*100).toString()+")";h>0&&p>0&&d>0&&(v=m),h==0&&p==0&&d==0&&(v=c);var g=!0;v==c&&(g=!1),u.push([[x],g]),e.fillStyle=v,g?e.fillRect(x,y,a,a):e.clearRect(x,y,a,a),x+=a-1}s.push(u),y+=a-1}}function d(e){function s(){f=window.setTimeout(function(){for(y=0;y<t.length;y++)for(x=0;x<t[0].length;x++){var e=t[y][x][1],o="rgb("+Math.floor(Math.random()*100).toString()+","+Math.floor(Math.random()*100).toString()+","+Math.floor(Math.random()*100).toString()+")",f=0;t[y-1]&&t[y-1][x-1]&&t[y-1][x-1][1]&&f++,t[y-1]&&t[y-1][x]&&t[y-1][x][1]&&f++,t[y-1]&&t[y-1][x+1]&&t[y-1][x+1][1]&&f++,t[y]&&t[y][x-1]&&t[y][x-1][1]&&f++,t[y]&&t[y][x+1]&&t[y][x+1][1]&&f++,t[y+1]&&t[y+1][x-1]&&t[y+1][x-1][1]&&f++,t[y+1]&&t[y+1][x]&&t[y+1][x][1]&&f++,t[y+1]&&t[y+1][x+1]&&t[y+1][x+1][1]&&f++;var l=f;n[y][x][1]=!1;if(e==1){if(l===r||l===i)n[y][x][1]=!0}else l===i&&(n[y][x][1]=!0);n[y][x][1]==1?(u.fillStyle=o,u.fillRect(x*a,y*a,a,a)):u.clearRect(x*a,y*a,a,a)}t=n,s()},100)}var t=e,n=e,r=2,i=3;l=!0,s()}var e=document.getElementById("my_face"),t=$(e).width(),n=$(e).height();e.width=t,e.height=n;var r=[],i=[],s=[],o=[],u,a=1,f,l=!1,c="rgba(0,0,0,0)";e&&e.getContext&&h(e),e.addEventListener("mouseover",function(){l||d(s)}),e.addEventListener("mouseout",function(){if(l=!0)l=!1;window.setTimeout(function(){l=!1,window.clearTimeout(f)},2e3)})});
+$(document).ready(function(){
+
+//###############
+//  face canvas
+//###############
+
+var canvas = document.getElementById('my_face');
+var canvas_width = $(canvas).width(); 
+var canvas_height= $(canvas).height(); 
+
+canvas.width = canvas_width; 
+canvas.height = canvas_height; 
+var next_gen = [];
+var changed = [];
+var pixel_matrix = [];
+
+var new_pixel_matrix = [];  
+var c;
+var pixel_size = 1
+var timer; 
+var is_active = false; 
+var white = 'rgba(0,0,0,0)'
+
+if (canvas && canvas.getContext){
+  runCanvas(canvas); 
+}
+
+function runCanvas(canvas){
+  c = canvas.getContext('2d');
+  var image = document.getElementById('my_face_avatar');
+  c.drawImage(image, 0,0, canvas_width, canvas_height)
+  pixelate_image(c);     
+}//runCanvas()
+
+canvas.addEventListener('mouseover',function(){
+  if(!is_active){
+    game_of_life(pixel_matrix);
+  }
+});
+
+canvas.addEventListener('mouseout', function(){
+  
+  if (is_active=true){
+    is_active=false  
+  }
+  
+  window.setTimeout(function(){
+    is_active=false      
+    window.clearTimeout(timer) 
+  }, 2000)
+
+})
+
+function pixelate_image(c){
+  var imageData = c.getImageData(0, 0, canvas_width, canvas_height);
+  var data = imageData.data
+  var contrast_level = 0;
+
+  for(y=0; y<canvas_height; y++){   
+    var pixel_row = [];
+    for(x=0; x<canvas_width; x++){
+      var rand=Math.floor(Math.random()*255)
+      var index = (y * canvas_width + x) * 4;
+      var r = data[index];
+      var g = data[index+1];
+      var b = data[index+2];
+      var fill_style = white
+      var rand_color = 'rgb('+Math.floor(Math.random()*100).toString()+','+Math.floor(Math.random()*100).toString()+','+Math.floor(Math.random()*100).toString()+')';
+      
+      if(r>0 && g>0 && b>0){fill_style = rand_color};
+      //var mid = 180
+      //var low = 0
+      
+      //if(r>low && r<mid && g>low && g<mid && b>low && b<mid ){fill_style = rand_color};
+      
+      //var mid = 180
+      //var low = 140
+      if(r==0 && g==0 && b==0 ){fill_style=white};
+      
+      var is_active = true 
+      if(fill_style==white){is_active = false;}
+      
+      pixel_row.push([[x],is_active]);
+      
+      c.fillStyle =fill_style 
+      if (is_active){c.fillRect( x, y, pixel_size, pixel_size )} 
+      else{c.clearRect( x, y, pixel_size, pixel_size )};
+      x=x+(pixel_size-1)
+    }
+    pixel_matrix.push(pixel_row)
+    y=y+(pixel_size-1)    
+  }
+}
+
+function game_of_life(pixel_matrix){ 
+  var old_matrix = pixel_matrix;
+  var new_matrix = pixel_matrix; 
+  var min = 2;
+  var max = 3;
+  is_active = true; 
+  stepThrough()
+  function stepThrough(){
+    timer = window.setTimeout(function(){
+        for(y=0; y<old_matrix.length; y++){
+          for(x=0; x<old_matrix[0].length; x++){
+            var old_state = old_matrix[y][x][1];
+            
+            var rand_color = 'rgb('+Math.floor(Math.random()*100).toString()+','+Math.floor(Math.random()*100).toString()+','+Math.floor(Math.random()*100).toString()+')';
+            
+            var n = 0;
+            if (old_matrix[y-1] && old_matrix[y-1][x-1] && old_matrix[y-1][x-1][1])	n++;
+            if (old_matrix[y-1] && old_matrix[y-1][x  ] && old_matrix[y-1][x  ][1])	n++;
+            if (old_matrix[y-1] && old_matrix[y-1][x+1] && old_matrix[y-1][x+1][1])	n++;
+            if (old_matrix[y  ] && old_matrix[y  ][x-1] && old_matrix[y  ][x-1][1])	n++;
+            if (old_matrix[y  ] && old_matrix[y  ][x+1] && old_matrix[y  ][x+1][1])	n++;
+            if (old_matrix[y+1] && old_matrix[y+1][x-1] && old_matrix[y+1][x-1][1])	n++;
+            if (old_matrix[y+1] && old_matrix[y+1][x  ] && old_matrix[y+1][x  ][1])	n++;
+            if (old_matrix[y+1] && old_matrix[y+1][x+1] && old_matrix[y+1][x+1][1])	n++;
+            
+            var live_neighbors = n; 
+  
+            new_matrix[y][x][1]=false;  
+            
+            if(old_state==true){
+              if (live_neighbors === min || live_neighbors === max){
+                new_matrix[y][x][1]=true;  
+              }
+            } else if(live_neighbors === max){
+                new_matrix[y][x][1]=true;                 
+            } 
+
+            if(new_matrix[y][x][1] == true){
+              c.fillStyle = rand_color
+              c.fillRect(x*pixel_size, y*pixel_size, pixel_size, pixel_size)                    
+            } else {
+              c.clearRect(x*pixel_size, y*pixel_size, pixel_size, pixel_size) 
+            }
+          }
+        }
+        old_matrix = new_matrix      
+        stepThrough(); 
+
+    }, 100); //setTimeout
+  }
+}
+
+/*
+function desaturate(r, g, b) {
+  var intensity = 0.3 * r + 0.59 * g + 0.11 * b;
+  var k = 1;
+  r = Math.floor(intensity * k + r * (1 - k));
+  g = Math.floor(intensity * k + g * (1 - k));
+  b = Math.floor(intensity * k + b * (1 - k));
+  return [r, g, b];
+}
+*/
+//##########################
+// project thumbnail canvas
+//##########################
+
+/*
+var thumbnails = $('.project_thumbnail_image');
+var thumb_canvases = $('.project_thumbnail_canvas');
+
+
+// here: remove the for loop. add an i argument to the function, draw the images on load, then iterate i+1 inside the onload event and recursively call the function again until i = length
+var run_thumbs = function(){
+ for(i=0; i<thumb_canvases.length; i++){
+    thumb_canvases[i].width = 75; 
+    thumb_canvases[i].height = 75; 
+    
+    thumb_canvases[i]
+    .getContext('2d')
+    .drawImage(thumbnails[i], 0,0, thumbnails[i].width, thumbnails[i].height);
+    
+    bw_pixelate_image(thumb_canvases[i].getContext('2d'));
+  }
+}
+
+run_thumbs(); 
+
+function bw_pixelate_image(c){
+  var pixel_size =5; 
+  var imageData = c.getImageData(0, 0, 75,75);
+  var data = imageData.data
+  
+  for(y=0; y<75; y++){   
+    for(x=0; x<75; x++){
+      var index = (y * 75 + x) * 4;
+      var r = Math.floor(data[index]);
+      var g = Math.floor(data[index+1]);
+      var b = Math.floor(data[index+2]);
+      var rgb = desaturate(r,g,b)
+
+      c.fillStyle = "rgb("+rgb[0]+","+rgb[1]+","+rgb[2]+")";
+      c.fillRect( x, y, pixel_size, pixel_size);
+
+      x=x+(pixel_size-1)
+    }
+    y=y+(pixel_size-1)    
+  }
+  
+}
+*/
+});
+
