6b9376ddce13ebcaf07d737faeafe53e4693dd8e
Jan Drewniak <jan.drewniak@gmail.com>
2014-02-17 13:17:11 -0500

    fixing xml error

--- site/templates/sitemap.php
+++ site/templates/sitemap.php
@@ -1,15 +1,14 @@
-<?php
-$ignore = array('sitemap', 'error');
+<?= xml('<?xml version="1.0" encoding="utf-8"?>');?>
+<? $ignore = array('sitemap', 'error');
 // send the right header
-header('Content-type: text/xml; charset="utf-8"');
-?>
+header('Content-type: text/xml; charset="utf-8"');?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	<?php foreach($pages->visible()->index() as $p): ?>
-	<?php if(in_array($p->uri(), $ignore)) continue ?>
+	<? foreach($pages->visible()->index() as $p): ?>
+	<? if(in_array($p->uri(), $ignore)) continue ?>
 	<url>
-		<loc><?php echo html($p->url()) ?></loc>
+		<loc><?= html($p->url()) ?></loc>
 		<lastmod><?php echo $p->modified('c') ?></lastmod>
 		<priority><?php echo ($p->isHomePage()) ? 1 : number_format(0.5/$p->depth(), 1) ?></priority>
 	</url>
-	<?php endforeach ?>  
+	<? endforeach?>  
 </urlset>
