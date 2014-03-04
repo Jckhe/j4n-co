e4bad07ce0f047e175926a6a7c21b7447906a1a1
Jan Drewniak <jan.drewniak@gmail.com>
2014-03-04 04:21:41 -0500

    adding google analytics

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -5,5 +5,16 @@
 <?= $page->custom_js() ?>
 
 </body>
+<!-- google analytics --> 
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-19507403-4', 'j4n.co');
+  ga('send', 'pageview');
+
+</script>
 
 </html>
