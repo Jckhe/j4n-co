eddf5de5d11adf02f1db40666e31174c91510763
Jan Drewniak <jan.drewniak@gmail.com>
2014-04-28 06:23:27 -0400

    v1

--- assets/config.rb
+++ assets/config.rb
@@ -2,6 +2,7 @@
 
 # General
 #require './sass_debug_overide'
+require 'autoprefixer-rails'
 
 output_style = :compressed
 line_comments = false
@@ -15,7 +16,15 @@ project_type="stand_alone"
 environment="production"
 sass_options = {:sourcemap => true }
 
-#livereload = true
+
+on_stylesheet_saved do |file|
+  css = File.read(file)
+  File.open(file, 'w') do |io|
+    io << AutoprefixerRails.process(css, browsers: ["last 4 version", "> 4%"])
+  end
+end
+
+livereload = true
 #sass_options = { :debug_info => [true, :replace => ['..', :with=> 'summ' ] }
 
 #debug_info = true
