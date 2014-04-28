5788c28f5ca9fcec0403d7a1ed93a64d03bec126
Jan Drewniak <jan.drewniak@gmail.com>
2014-04-28 06:23:41 -0400

    v1

---
+++ site/snippets/contactform.php
@@ -0,0 +1,79 @@
+<?php
+
+$form = new contactform(array(
+  'to'       => 'Jan <jan.drewniak@gmail.com>',
+  'from'     => 'j4n.co Contact Form <contact-form@j4n.co>',
+  'subject'  => 'j4n.co Contact Form ',
+  'goto'     => $page->url() . '/status:thank-you#contact-me'
+));
+
+?>
+<section id="contactform">
+
+  <?php if(param('status') == 'thank-you'): ?>
+
+
+  <h1>Thanks for getting in touch!</h1>
+  <p class="contactform-text" style="margin-bottom: 200px;">
+    <strong>
+      I'll respond to your email within 24 hours.
+      </strong>
+  </p>
+  
+  <?php else: ?>
+
+  <h1>Contact Me</h1>
+  
+  <form action="#contactform" method="post">
+    
+      <?php if($form->isError('send')): ?>
+      <p class="contactform-alert">
+        <strong class="error">
+          The email could not be sent. Please try again later.
+        </strong>
+      </p>
+      <?php elseif($form->isError()): ?>
+      <p class="contactform-alert">
+        <strong class="error"> 
+          The form could not be submitted. Please fill in all fields correctly.
+        </strong>
+      </p>
+      <?php endif ?>
+  
+      <div class="contactform-field-half">
+        <label class="contactform-label" for="contactform-name">Your Name 
+        <?php if($form->isRequired('name')) echo '*' ?> 
+        </label>
+        <?php if($form->isError('name')): ?>
+          <small class="error">Please enter a name</small>
+        <?php endif ?>
+        <input class="contactform-input" type="text" id="contactform-name" name="name" placeholder="your name" value="<?php echo $form->htmlValue('name') ?>" />
+      </div>
+  
+      <div class="contactform-field-half">
+        <label class="contactform-label" for="contactform-email">
+        Your Email adresse 
+        <?php if($form->isRequired('email')) echo '*' ?> 
+        </label>
+        <?php if($form->isError('email')): ?>
+          <small class="error">Please enter a valid email address</small>
+        <?php endif ?>
+        <input class="contactform-input" type="text" id="contactform-email" name="email" placeholder="your email" value="<?php echo $form->htmlValue('email') ?>" />
+      </div>
+  
+      <div class="contactform-field">
+        <label class="contactform-label" for="contactform-text">Message 
+        <?php if($form->isRequired('text')) echo '*' ?> 
+        </label>
+        <?php if($form->isError('text')): ?>
+          <small class="error">Please enter your text</small>
+        <?php endif ?>
+        <textarea class="contactform-input" name="text" id="contactform-text"><?php echo $form->htmlValue('text') ?></textarea>
+      </div>
+
+      <input class="contactform-button light_green" type="submit" name="submit" value="Send" />    
+  </form>
+  
+  <?php endif ?>
+
+</section>
--- site/templates/home.php
+++ site/templates/home.php
@@ -3,83 +3,160 @@
 
 
 <div class="home-column-left">
-  <div class="home-title">
-    <div class="home-avatar">
+  <div class="home-title flip-container">
+    <div class="flipper animated">
+      <div class="home-avatar-front flipper-front">
+      </div>
+      <div class="home-avatar-back flipper-back"><a class="about-link" href="#about-me">more about me...</a></div>
     </div>
   </div>
-  <p class="home-intro">
-    Hey there, I’m an interaction designer from Toronto, currently located in Warsaw. I design and build stuff on the web. 
+  <p class="home-intro floating-p animated bounceInLeft">
+    Hey there, I’m Jan. I design and build stuff for the web. 
   </p>
-  <p class="left-headline">
-    <span>
+  <p class="left-headline  animated bounceInLeft">
+    <span class="floating-p">
       In the past I’ve helped people
     </span>
   </p>
+  <p class="floating-p home-how animated bounceInLeft"> 
+    I love creating rich interactions for the web and experimenting with interfaces, wether it be for desktop, mobile or screens we haven't yet imagined.     </br>
+    <br/>
+    <button class="green-contact aqua">
+      <a href="#contact-me">get in touch</a>
+    </button>
+  </p>
+
 </div>
 
 <div class="home-column-right">
-
-  <div class="home-portfolio">
-    <p class="right-headline cf">
-      <span>
-        manage their work schedules
-      </span>
-    </p>
-    <div class="portfolio-image">
-      <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
-      <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+  <div class="home-column-right-content">
+    <div class="home-portfolio">
+      <p class="right-headline cf">
+        <span class='floating-p animated bounceInLeft'>
+          manage their schedules at work,
+        </span>
+      </p>
+      <div class="portfolio-image">
+        <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
+        <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.png"/>
+      </div>
     </div>
-  </div>
 
-  <div class="home-portfolio">
-    <p class="right-headline cf">
-      <span>
-        share ideas with their co-workers,
-      </span>
-    </p>
-    <div class="portfolio-image">
-      <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
-      <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+    <div class="home-portfolio">
+      <p class="right-headline cf">
+        <span class='floating-p animated bounceInLeft'>
+          share ideas with their co-workers,
+        </span>
+      </p>
+      <div class="portfolio-image">
+        <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
+        <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+      </div>
     </div>
-  </div>
 
 
-  <div class="home-portfolio">
-    <p class="right-headline cf">
-      <span>
-        buy art books
-      </span>
-    </p>
-    <div class="portfolio-image">
-      <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
-      <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+    <div class="home-portfolio">
+      <p class="right-headline cf">
+        <span class='floating-p animated bounceInLeft'>
+          sell art & photography books online,
+        </span>
+      </p>
+      <div class="portfolio-image">
+        <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
+        <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+      </div>
     </div>
-  </div>
 
-  <div class="home-portfolio">
-    <p class="right-headline cf">
-      <span>
-        promote local designers
-      </span>
-    </p>
-    <div class="portfolio-image">
-      <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
-      <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+    <div class="home-portfolio">
+      <p class="right-headline cf">
+        <span class='floating-p animated bounceInLeft'>
+          promote local jewelery & fashion designers,
+        </span>
+      </p>
+      <div class="portfolio-image">
+        <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
+        <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+      </div>
     </div>
-  </div>
 
-  <div class="home-portfolio">
-    <p class="right-headline cf">
-      <span>
-        create photoblogs 
-      </span>
-    </p>
-    <div class="portfolio-image">
-      <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
-      <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+    <div class="home-portfolio">
+      <p class="right-headline cf">
+        <span class='floating-p animated bounceInLeft'>
+          create unique photoblogs, 
+        </span>
+      </p>
+      <div class="portfolio-image">
+        <img class="portfolio-hover-image" src="/j4n-co/assets/images/home-skedx-overlay.jpg"/>
+        <img class="portfolio-initial-image" src="/j4n-co/assets/images/home-skedx.jpg"/>
+      </div>
     </div>
+
+
+    <div id="contact-me" class="home-portfolio">
+      <p class="right-headline cf">
+        <span class='floating-p animated bounceInLeft'>
+          like you.
+        </span>
+      </p>
+      <?php snippet('contactform') ?>      
+    </div>    
   </div>
 </div>
 
 
 <? snippet('footer') ?>
+
+<script> 
+$(document).ready(function(){
+
+var didScroll = false;
+ 
+var headlineOffsets = []
+var initialOffset = $('.home-column-right-content').offset().top
+var rightHeadlines = $('.right-headline')
+
+$('.right-headline').each(function(index, element){
+  headlineOffsets.push($(element).offset().top-11)
+})
+
+$(window).scroll(function(event) {
+  //didScroll = true;
+  scrollCallback()
+});
+ 
+/*setInterval(function() {
+  if ( didScroll ) {
+    didScroll = false;
+    //scrollCallback()
+  }
+}, 250);
+*/
+function scrollCallback(){
+  
+  var currentOffset = $(window).scrollTop() + initialOffset; 
+  var nearbyHeadline;
+  var absoluteDiff;
+  var diff; 
+
+  for (i=0; i<headlineOffsets.length; i++){
+
+    diff = currentOffset - headlineOffsets[i]
+    
+    absoluteDiff = Math.abs(diff)
+    
+    if (absoluteDiff < 50){
+      alert(i)
+      nearbyHeadline = rightHeadlines[i]
+      $(nearbyHeadline).css({
+        WebkitTransform: 'translateY('+diff+'px)',
+        MozTransform: 'translateY('+diff+'px)',
+        msTransform: 'translateY('+diff+'px)',
+        transform: 'translateY('+diff+'px)'
+      })
+      break;
+    }
+  }
+}
+
+});//document.ready
+</script> 
