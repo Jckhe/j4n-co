0065a53616ed251291a70e7e1001904ac8b1de05
Jan Drewniak <jan.drewniak@gmail.com>
2014-05-20 12:10:53 -0400

    adding codepen social icon

--- site/snippets/site_nav.php
+++ site/snippets/site_nav.php
@@ -1,5 +1,3 @@
-<!--<input class="hidden_input" type="checkbox" id="expand_me" /> -->
-
 <header class='top_bar dark_grey' itemscope itemtype='http://schema.org/WPHeader'>
 
   <nav class='sitenav' itemscope itemtype='http://schema.org/SiteNavigationElement'>
@@ -47,39 +45,19 @@
       </li>
     <? $color_i = min($color_i+1, count($colors)-1 ); ; ?>
     <? endforeach ?>
-      <!--
-      <li class="me section">
-        <label for="expand_me" class="my_pixelated_face"></label>
-        <label for="expand_me" class="my_face"></label>
-      </li>
-      -->
-    </ul>
 
+    </ul>
 
   </nav>        
   
-  <!--
-  
-  <div class="expanded_bar"></div>
-
-  <div class="expanded_content">
-    <h1 class="white_text">Hi there, I'm Jan.</h1>
-    <p class="white_text">
-    I'm a designer/developer — let's pause on that for a second — You might be thinking "What? artsy people don't code" but the computer has always been my canvas. 
-    I enjoy <a href="<?= $pages->find("lab")->url() ?>">experimenting</a> with HTML/CSS/JS and making engaging <a href="<?= $pages->find("portfolio")->url() ?>">web-things</a> (sites/apps...whatever).
-    </p>
-  </div>
--->
   <a class='tagline iblock' href='<?= $pages->find("home")->url() ?>'>
     <h1 itemprop='headline'>
       Jan Drewniak | <span itemprop="description">Art + Code for fun and profit</span>
     </h1>
   </a>
   
-  
-   
-  <a class="icon-social github" target="_BLANK" href="https://github.com/j4n-co">
-    github
+  <a class="icon-social codepen" target="_BLANK" href="http://codepen.io/j4n">
+    codepen
   </a>
   <a class="icon-social linkedin" target="_BLANK" href="http://ca.linkedin.com/in/jandrewniak">
     linkedin
